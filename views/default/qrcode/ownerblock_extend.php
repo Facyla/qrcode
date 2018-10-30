266c13665f1a58ede3093749b5bea25a67ce4054
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2018-10-30 18:45:39 +0100

    Production version for Elgg 1.12

--- views/default/qrcode/ownerblock_extend.php
+++ views/default/qrcode/ownerblock_extend.php
@@ -1,9 +1,9 @@
 <?php
 $title = elgg_echo('qrcode:page:title');
 
-$content = elgg_view('output/qrcode', array('qrcode_url' => full_url()));
+$content = elgg_view('output/qrcode', array('qrcode_url' => current_page_url()));
 
-// Render QR code popup (called ny menu)
+// Render QR code popup (called by menu)
 echo elgg_view_module('popup', $title, $content, array(
 		'id' => "elgg-popup-qrcode-page",
 		'class' => 'hidden clearfix developers-content-thin',
